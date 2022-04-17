dc95b9efdde8bdb7ffe5ce1417607da6c0eb3671
efjay <efjaymania@gmail.com>
2022-04-17 22:50:17 +0100

    added skills section

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
-gsap.registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(ScrollTrigger)
 
-gsap.from("#me", {duration:1, y: 70, opacity: 0.4})
-gsap.from("#header", {duration:1, y: 70, opacity: 0.4})
-gsap.from("#full", {duration: 0.5, x: 70, opacity: 0, delay: 1.5})
-gsap.from("#name", {duration: 0.5, x: 50, opacity: 0, delay: 1.5})
-gsap.from("#head", {duration: 0.5, x: -100, opacity: 0, delay: 1.5})
-gsap.from("#scroll", {duration:1, y: -70, opacity: 0.4})
+// gsap.from("#me", {duration: 0.5, y: 70, opacity: 0.4})
+// gsap.from("#header", {duration: 0.5, y: 70, opacity: 0.4})
+// gsap.from("#full", {duration:  0.5, x: 70, opacity: 0, delay:  0.5})
+// gsap.from("#name", {duration:  0.5, x: 50, opacity: 0, delay:  0.5})
+// gsap.from("#head", {duration:  0.5, x: -100, opacity: 0, delay:  0.5})
+// gsap.from("#scroll", {duration: 0.5, y: -70, opacity: 0.4})
 
-gsap.from("#contentBox", {duration: 1, skewZ: 20, skewY: -10, rotateX: 70, ease: 'rough'})
-gsap.from("#scroll", {duration:1, y: -70, opacity: 0.4})
+// gsap.from("#contentBox", {scrollTrigger: {trigger: "#about", toggleActions: "restart none none none", start: "top 10%"},duration: 2, skewY: 20, skewY: -10, rotateX: 110, rotateY: 90, opacity: 0,ease: 'rough'})
+// gsap.from("#aboutHeader", {scrollTrigger: {trigger: "#about", toggleActions: "restart none none none", start: "top 10%" },duration:2, x: 70, opacity: 0, rotateY: 90})
 
