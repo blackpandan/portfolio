112d73a05194d21da4f225bc7b82b50afc856d17
blackpandan <faithgstc54@gmail.com>
2022-04-24 19:02:08 +0100

    added real project

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,23 @@
 // gsap.from("#contentBox", {scrollTrigger: {trigger: "#about", toggleActions: "restart none none none", start: "top 10%"},duration: 2, skewY: 20, skewY: -10, rotateX: 110, rotateY: 90, opacity: 0,ease: 'rough'})
 // gsap.from("#aboutHeader", {scrollTrigger: {trigger: "#about", toggleActions: "restart none none none", start: "top 10%" },duration:2, x: 70, opacity: 0, rotateY: 90})
 
+
+/*
+    title
+    desctiption
+    langs
+    url => live, code
+*/
+
+let works = [
+    {
+        title: "skar strokes",
+        description: "",
+        langs: ["scss", "Vue.js", "Nuxt"],
+        url: {
+            live: "https://skarstrokes.com",
+            code: ""
+        }
+
+    }    
+]
